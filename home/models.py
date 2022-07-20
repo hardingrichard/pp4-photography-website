d2dec08db863bcbc919e096eef4e7ccc5f20bc49
Richard Harding <richard.jharding@hotmail.co.uk>
2022-07-20 00:48:52 +0000

    add project class to home models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='/workspace/pp4-photography-website/images/')
