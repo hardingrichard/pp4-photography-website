dd6047dadc1252f57f5c164414e0a01882db5909
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-21 21:14:27 +0000

    Amend Project class to relative path and folder location

--- home/models.py
+++ home/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='/workspace/pp4-photography-website/images/')
+    image = models.ImageField(upload_to='workspace/pp4-photography-website/media/')
