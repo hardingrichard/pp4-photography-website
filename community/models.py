17a5896cab5da5c6587135569de0ef07fa94f2d3
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-22 20:33:45 +0000

    Add TaggableManager import from taggit

--- community/models.py
+++ community/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from taggit.managers import TaggableManager
+
 # Photoshare model for the community app
 class Photoshare(models.Model):
     title = models.CharField(max_length=45)
