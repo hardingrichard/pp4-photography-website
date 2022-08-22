02fbb839973259c4402f4b8a2ec3cbb083074271
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-22 22:14:25 +0000

    Amend Photoshare class with CloudinaryField

--- community/models.py
+++ community/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth import get_user_model
 
 # 3rd party
 from taggit.managers import TaggableManager
+from cloudinary.models import CloudinaryField
 
 # Photoshare model for the community app
 class Photoshare(models.Model):
     title = models.CharField(max_length=45)
     description = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='media/')
+    image = CloudinaryField('image', default='placeholder')
     submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     # Method for how each job will display in the admin area
