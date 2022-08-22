5ea77faadd7f75e9532f8c69fee2fecc8fae8e9a
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-22 20:30:43 +0000

    Add photoshare model for the community app

--- community/models.py
+++ community/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+# Photoshare model for the community app
+class Photoshare(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='media/')
+    submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    # Method for how each job will display in the admin area
+    def __str__(self):
+        return self.title
