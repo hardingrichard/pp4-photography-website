e66343cdab2598b38fc832db14e43f27cef0f88c
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-22 20:17:51 +0000

    Add variable to make tags for app case insensitive

--- photography_project/settings.py
+++ photography_project/settings.py
@@ -42,7 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
     'cloudinary',
+    'taggit',
+
+    # Custom apps
     'home',
     'about',
     'gallery',
@@ -144,3 +149,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django-taggit tags sensitivity
+TAGGGIT_CASE_INSENSITIVE = True
