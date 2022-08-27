4a4e38d96820b2d6d1562db1585fefdadf458654
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 22:17:47 +0000

    Add ability for users to create a new log in

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+# Allows the user to register and create a new login
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('image:list')
+
+    # Log the user in before redirecting
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+        user = authenticate(
+            username = form.cleaned_data["username"],
+            password = form.cleaned_data["password1"],
+        )
+
+        login(self.request, user)
+        return to_return
+
