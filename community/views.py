7020bf6b43424e4fe24681178d81429c20b556d7
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 13:35:44 +0000

    Add login validation for creating photo sharing

--- community/views.py
+++ community/views.py
@@ -48,3 +48,5 @@ class Photoshare(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.submitter = self.request.user
         return super().form_valid(form)
+
+class
