b409ea95ad35d4710654e03465b6503e3b99c609
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 23:26:16 +0000

    Add tags variable for TaggableManager

--- community/models.py
+++ community/models.py
@@ -11,6 +11,7 @@ class Photoshare(models.Model):
     description = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     image = CloudinaryField('image', default='placeholder')
+    tags = TaggableManager()
     submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     # Method for how each job will display in the admin area
