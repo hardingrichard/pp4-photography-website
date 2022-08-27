d839db7b919cb3514931605e3f50d3fb13c68d13
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 20:45:30 +0000

    Add url configurations

--- community/urls.py
+++ community/urls.py
@@ -12,3 +12,14 @@ from .views import (
     PhotoshareUpdateView
 )
 
+# Declares namespace of the app
+app_name = 'image'
+
+urlpatterns = [ 
+    path('', PhotoshareListView.as_view(), name = 'list'),
+    path('image/<int:pk>/', PhotoshareDetailView.as_view(), name = 'detail'),
+    path('tag/<slug:tag>/', PhotoshareTagListView.as_view(), name = 'tag'),
+    path('image/create/', PhotoshareCreateView.as_view(), name = 'create'),
+    path('image/<int:pk>/update/' PhotoshareUpdateView.as_view(), name = 'update'),
+    path('image/<int:pk>/delete/' PhotoshareDeleteView.as_view(), name = 'delete'),
+]
