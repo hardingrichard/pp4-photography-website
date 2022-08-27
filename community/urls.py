05d2b98aa4d24081b6400572d1ce4ed74a2f2ba7
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 19:05:53 +0000

    Remove urlpatterns as added to wrong file in error

--- community/urls.py
+++ community/urls.py
@@ -12,10 +12,3 @@ from .views import (
     PhotoshareUpdateView
 )
 
-# Include all URL patterns to project
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Community photosharing app
-    path('', include('community.urls')),
-    path('', views.index, name='index'),
-]
