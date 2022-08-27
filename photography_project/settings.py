4d4127bd4894512ce3a03d9387231df824587ed6
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 22:44:07 +0000

    Amend DIRS key to search for templates in template folder

--- photography_project/settings.py
+++ photography_project/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'photography_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
