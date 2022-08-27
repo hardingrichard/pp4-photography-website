f89d2da4a4079b5ef6370f7e44a36ba0f137756e
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 23:07:15 +0000

    Fix add views import for LoginView for NameError

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import CreateView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
 
