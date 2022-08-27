d2e0eb497814499ae228e1448bc052ccb777171a
Richard Harding <richard.jharding@hotmail.co.uk>
2022-08-27 19:00:38 +0000

    Add views and functions from views.py

--- community/urls.py
+++ community/urls.py
@@ -2,6 +2,16 @@ from django.urls import path, include
 from django.contrib import admin
 from Photoshare import views
 
+# Import views and functions from views.py
+from .views import (
+    PhotoshareListView,
+    PhotoshareTagListView,
+    PhotoshareDeleteView, 
+    PhotoshareDetailView,
+    PhotoshareCreateView,
+    PhotoshareUpdateView
+)
+
 # Include all URL patterns to project
 urlpatterns = [
     path('admin/', admin.site.urls),
