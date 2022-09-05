393c0fa31a878b689a72fc92c8971073ab7fe31f
Richard Harding <richard.jharding@hotmail.co.uk>
2022-09-05 20:47:24 +0000

    Add DATABASES variable set to default

--- photography_project/settings.py
+++ photography_project/settings.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = 'photography_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Temporary bug fix - commented out as causing Fatal Error. 
